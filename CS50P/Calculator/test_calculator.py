f452f2e6c3737e89d2be5cab7c728c470dec439f
Fausto Ferrara <faustoferrara@gmail.com>
2023-01-09 20:59:48 +0100

    Add UnitTest examples

--- CS50P/Calculator/test_calculator.py
+++ CS50P/Calculator/test_calculator.py
@@ -1,4 +1,5 @@
 from calculator import square
+import pytest
 
 
 def main():
@@ -32,12 +33,30 @@ def main():
 #         print("0 squared was not 0")
 
 # version 3 - using UnitTest:
+# def test_square():
+#     assert square(2) == 4
+#     assert square(3) == 9
+#     assert square(0) == 0
 
 
-def test_square():
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
     assert square(0) == 0
+
+
+# testing that the program will run a TypeError if a 'str' is inserted, rather than an 'int':
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
 
 
 if __name__ == "__main__":
