cd4041bdcec3400e193761e8f437bbd5bcaa28e6
Fausto Ferrara <faustoferrara@gmail.com>
2023-02-08 21:59:38 +0100

    Add Notes within CS50P Exercises files weeks 0 and 1

--- CS50P/ExercisesWeek_0/einstein.py
+++ CS50P/ExercisesWeek_0/einstein.py
@@ -1,3 +1,23 @@
+"""
+Einstein
+
+Even if you haven't studied physics (recently or ever!), you might have heard that
+, wherein:
+E = represents energy (measured in Joules),
+m = represents mass (measured in kilograms), and
+c*2 = represents the speed of light (measured approximately as 300000000 meters per second),
+per Albert Einstein et al. Essentially, the formula means that mass and energy are equivalent.
+
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+
+NOTE:
+Recall that input returns a str, per docs.python.org/3/library/functions.html#input.
+Recall that int can convert a str to an int, per docs.python.org/3/library/functions.html#int.
+Recall that Python comes with several built-in functions, per docs.python.org/3/library/functions.html.
+
+"""
+
+
 def main():
     mass = int(input("Enter mass in kilograms: "))
     c = 300000000  # speed of light in meters per second
