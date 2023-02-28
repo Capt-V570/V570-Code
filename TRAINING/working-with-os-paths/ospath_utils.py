6074dbe63db66137ca929a03e74ee26fdaf2d399
Fausto Ferrara <faustoferrara@gmail.com>
2023-02-28 18:10:20 +0100

    Adjust file in Working-with-os-path

--- TRAINING/working-with-os-paths/ospath_utils.py
+++ TRAINING/working-with-os-paths/ospath_utils.py
@@ -6,8 +6,12 @@ from datetime import date, time, timedelta
 import time
 import platform
 
+# print(os.name)
+# print(platform.system())
+# print(platform.release())
 
-def main():
+
+def os_and_path_operations():
     # Print the name of the OS:
     print(os.name)
     # Print platform name
@@ -20,6 +24,25 @@ def main():
     print("Item is a file:", path.isfile("textfile.txt"))
     print("Item is a directory:", path.isdir("textfile.txt"))
 
+    # Work with file paths:
+    print("Item's path:", path.realpath("textfile.txt"))
+    # Separate file name from path:
+    print("Item's path and name:", path.split(path.realpath("textfile.txt")))
 
-if __name__ == "__main___":
-    main()
+
+def time_operations():
+    # Get the modification time:
+    t = time.ctime(path.getmtime("textfile.txt"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
+
+    # Calculate how long ago a file was modified:
+    td = datetime.datetime.now() - datetime.datetime.fromtimestamp(
+        path.getmtime("textfile.txt")
+    )
+    print("It has been", td, "since the file was last modified")
+    print("or in seconds:", td.total_seconds())
+
+
+os_and_path_operations()
+time_operations()
