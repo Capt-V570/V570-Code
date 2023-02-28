293cb8d018a78d10a561c9c7289a3a28c23e5095
Fausto Ferrara <faustoferrara@gmail.com>
2023-02-28 17:45:48 +0100

    Add Working with OS Paths

--- TRAINING/working-with-os-paths/ospath_utils.py
+++ TRAINING/working-with-os-paths/ospath_utils.py
@@ -4,11 +4,21 @@ from os import path
 import datetime
 from datetime import date, time, timedelta
 import time
+import platform
 
 
 def main():
-    # print name of OS:
+    # Print the name of the OS:
     print(os.name)
+    # Print platform name
+    print(platform.system())
+    # Print version of OS:
+    print(platform.release())
+
+    # Check for item existence:
+    print("Item exists: ", str(path.exists("textfile.txt")))
+    print("Item is a file:", path.isfile("textfile.txt"))
+    print("Item is a directory:", path.isdir("textfile.txt"))
 
 
 if __name__ == "__main___":
